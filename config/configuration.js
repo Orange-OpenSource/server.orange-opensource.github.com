b5be8a3699202a1ff961e5de566229f630327fb6
ArthurHlt <arthurh.halet@gmail.com>
2016-08-12 10:52:16 +0200

    make it cloudfoundry ready

--- config/configuration.js
+++ config/configuration.js
@@ -1,37 +1,43 @@
 // node_env can either be "development" or "production"
 var node_env = process.env.NODE_ENV || "development";
 
+var isCF = require('is-cloudfoundry');
+var cfenv = require("cfenv");
+
 // Port to run the app on. 8000 for development
 // (Vagrant syncs this port)
 // 80 for production
 var default_port = 8000;
-if(node_env === "production") {
-  default_port = 5000;
+if (node_env === "production") {
+    default_port = 5000;
 }
 
 var server = {
-	name: 'server.adobe.github.com'
+    name: 'server.adobe.github.com'
 };
+if (isCF) {
+    var appEnv = cfenv.getAppEnv();
+    server.name = appEnv.url;
+}
+function crossOrigin(req, res, next) {
+    var oneof = false;
+    if (req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if (req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if (req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if (oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
 
-function crossOrigin(req,res,next){
-  var oneof = false;
-  if(req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-method']) {
-      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-headers']) {
-      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-      oneof = true;
-  }
-  if(oneof) {
-      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-  }
-
-  next();
+    next();
 }
 
 var dir = __dirname;
